8d08e0d7109ae72a60cff0196345ad36e64cbcc2
CyberSurfer108 <anthony.hennegan@gmail.com>
2026-07-04 10:55:03 -0700

    Apply user input to library project

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ library_name = "Bailey's Books and Bargains"
 librarian = "Anthony"
 book_count = 20
 is_open = True
+
+print(f"Welcome to {library_name}")
+reader_name = input("What is your name? ")
+print(f"Hello {reader_name}.")
+view_selection = input("Would you like to view our book selection? ")
