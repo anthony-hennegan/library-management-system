08cc8e7281a669cafa26705e565b2f7f2ef69d4c
CyberSurfer108 <anthony.hennegan@gmail.com>
2026-07-04 15:34:16 -0700

    Apply conditionals to library project

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ print(f"Welcome to {library_name}")
 reader_name = input("What is your name? ")
 print(f"Hello {reader_name}.")
 view_selection = input("Would you like to view our book selection? ")
+
+if view_selection == "yes":
+    print("Ok, here is what we have.")
+elif view_selection == "no":
+    print("No worries. Have a nice day!")
+else:
+    print("Please type yes or no.")
