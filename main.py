42afe6c162c6bcc09348554f479baab3fa082965
CyberSurfer108 <anthony.hennegan@gmail.com>
2026-07-06 07:35:42 -0700

    Apply comparison operators to library project

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ library_name = "Bailey's Books and Bargains"
 librarian = "Anthony"
 book_count = 20
 is_open = True
+checkout_limit = 3
 
 print(f"Welcome to {library_name}")
 reader_name = input("What is your name? ")
@@ -17,8 +18,8 @@ if cleaned_selection_response == "yes":
     requested_books = input("How many books would you like to check out? ")
     requested_books = int(requested_books)
 
-    if requested_books > 3:
-        print("Sorry, you can check out no more than 3 books.")
+    if requested_books > checkout_limit:
+        print(f"Sorry, you can check out no more than {checkout_limit} books.")
     else:
         print("Perfect. Let's get you fixed up.")
         
