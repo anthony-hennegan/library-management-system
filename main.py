f2f6dca0139ab5adbe94a823cc3303f9043d32fe
CyberSurfer108 <anthony.hennegan@gmail.com>
2026-07-06 18:51:51 -0700

    Apply logical operators to libray project

--- main.py
+++ main.py
@@ -10,20 +10,24 @@ formatted_name = reader_name.strip().title()
 
 print(f"Hello {formatted_name}.")
 
+if not is_open:
+    print("The library is closed.")
+    
 view_selection = input("Would you like to view our book selection? ")
 cleaned_selection_response = view_selection.strip().lower()
 
-if cleaned_selection_response == "yes":
+if cleaned_selection_response == "yes" or cleaned_selection_response == "y":
     print("Ok, great.")
     requested_books = input("How many books would you like to check out? ")
-    requested_books = int(requested_books)
+    requested_books = int(requested_books.strip())
 
-    if requested_books > checkout_limit:
-        print(f"Sorry, you can check out no more than {checkout_limit} books.")
-    else:
+    if requested_books <= checkout_limit and is_open:
         print("Perfect. Let's get you fixed up.")
+    else:
+        print("Checkout is not available.")
         
-elif cleaned_selection_response == "no":
+        
+elif cleaned_selection_response == "no" or cleaned_selection_response == "n":
     print("No worries. Have a nice day!")
 else:
     print("Please type yes or no.")
