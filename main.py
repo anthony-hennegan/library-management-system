4df2955cebd2b1048c2ee78a17155bfa60ad5975
CyberSurfer108 <anthony.hennegan@gmail.com>
2026-07-12 08:33:04 -0700

    Add logic functions to library project:

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# State
 books = [
     {
         "title": "The Alchemist",
@@ -21,37 +22,52 @@ book_count = len(books)
 is_open = True
 checkout_limit = 3
 
+# Logic Functions
+def show_welcome_message(name):
+    print(f"Welcome to {name}")
+    
+def greet_reader(name):
+    print(f"Hello {name}.")
 
-print(f"Welcome to {library_name}")
+def format_name(name):
+    return name.strip().title()
+
+def format_response(response):
+    return response.strip().lower()
+
+def display_books(book_list):
+    for book in book_list:
+        if book["checked_out"]:
+            print(f"- {book['title']} by {book['author']} ---- Unavailable")
+        else:
+            print(f"- {book['title']} by {book['author']} ---- Available")
+            
+# Render Section    
+show_welcome_message(library_name)
+
 reader_name = input("What is your name? ")
-formatted_name = reader_name.strip().title()
+reader_name = format_name(reader_name)
 
-print(f"Hello {formatted_name}.")
+greet_reader(reader_name)
 
 if not is_open:
     print("The library is closed.")
 
 print("")
-view_selection = input("Would you like to view our book selection? ")
-cleaned_selection_response = view_selection.strip().lower()
+view_selection_response = input("Would you like to view our book selection? ")
+view_selection_response = format_response(view_selection_response)
 
-if cleaned_selection_response == "yes" or cleaned_selection_response == "y":
+if view_selection_response == "yes" or view_selection_response == "y":
     print("Ok, great. Below is our selection.")
     print("")
     print("Bailey's Books")
     print("----------------")
+    display_books(books)
     
-    for book in books:
-        if book["checked_out"]:
-            print(f"- {book['title']} by {book['author']} ---- Unavailable")
-        else:
-            print(f"- {book['title']} by {book['author']} ---- Available")
-        
- 
     print("") 
     print(f"We have {book_count} books in our library.")
     requested_books = input("How many books would you like to check out? ")
-    requested_books = int(requested_books.strip())
+    requested_books = int(format_response(requested_books))
 
     if requested_books <= checkout_limit and is_open:
         print("Perfect. Let's get you fixed up.")
@@ -59,7 +75,7 @@ if cleaned_selection_response == "yes" or cleaned_selection_response == "y":
         print("Checkout is not available.")
         
         
-elif cleaned_selection_response == "no" or cleaned_selection_response == "n":
+elif view_selection_response == "no" or view_selection_response == "n":
     print("No worries. Have a nice day!")
 else:
     print("Please type yes or no.")
